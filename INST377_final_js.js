b9b51324cce1a71d0ae52b72f7dc7a555cd489b5
brandont28 <brandont@terpmail.umd.edu>
2025-05-14 17:22:25 -0400

    Added weapons and Agent Slider

--- INST377_final_js.js
+++ INST377_final_js.js
@@ -1,44 +1,104 @@
-//Fetch Weapons
-function getWeapons () {
-    fetch(`https://valorant-api.com/v1/weapons/{weaponUuid}`)
-    .then((resp) => resp.json())
-    .then((data) => {
-    });
+//Fetches Agents
+function getAgents () {
+    fetch(`https://valorant-api.com/v1/agents`)
+        .then((resp) => resp.json())
+        .then((data) => {
+            const agents = data.data;
+            //Removes duplicate Sova agent as recommended by API
+            const filteredAgents = agents.filter(agent => agent.isPlayableCharacter);
+            const agentSlides = document.getElementById('agentSlides');
+            console.log(filteredAgents);
+
+            filteredAgents.forEach(agent => {
+                const div = document.createElement("div");
+                const img = document.createElement("img");
+                const name = document.createElement("h2");
+                name.textContent = agent.displayName;
+                img.src = agent.fullPortraitV2;
+                div.appendChild(name);
+                div.appendChild(img);
+                agentSlides.appendChild(div);
+            });
+
+            const slider = simpleslider.getSlider({
+                delay: 10
+            });
+
+            document.getElementById('previousAgent').addEventListener('click', () => {
+                slider.prev();
+            });
+
+            document.getElementById('nextAgent').addEventListener('click', () => {
+                slider.next();
+            });
+        });
 }
 
+
+
+//Fetch Weapons
+function getTopWeapons () {
+    fetch(`https://valorant-api.com/v1/weapons`)
+    .then((resp) => resp.json())
+    .then((data) => {
+        const vandal = "9c82e19d-4575-0200-1a81-3eacf00cf872";
+        const phantom = "ee8e8d15-496b-07ac-e5f6-8fae5d4c7b1a";
+        const operator = "a03b24d3-4319-996d-0f8c-94bbfba1dfc7";
+        const weaponsList = document.getElementById('weapons');
+
+        //Only selects these 3 weapons as they are the most known/used
+        const top3Weapons = data.data.filter(weapon =>
+        [vandal, phantom, operator].includes(weapon.uuid)
+        );
+
+        top3Weapons.forEach(weapon => {
+            const div = document.createElement("div");
+            const img = document.createElement("img");
+            img.src = weapon.displayIcon;
+            const name = document.createElement("span");
+
+            name.textContent = weapon.displayName;
+            div.appendChild(img);
+            div.appendChild(name);
+            weaponsList.appendChild(div);
+
+        })
+    });
+}
 
 //Fetch Competitive Ranks API
 function getRanks () {
     fetch(`https://valorant-api.com/v1/competitivetiers`)
     .then((resp) => resp.json())
     .then((data) => {
+        //Removes first 3 as they are non-ranks
         const tiers = data.data[data.data.length - 1].tiers;
         const tierNames = tiers.slice(3);
         const ranks = document.getElementById('ranks');
 
+        //Creates elements to display rank names and their icons
         tierNames.forEach(tier => {
             const div = document.createElement('div');
             const rankName = tier.tierName;
             const img = document.createElement('img');
             const rankIcon = tier.smallIcon;
-            console.log(tier.smallIcon);
 
             img.src = rankIcon;
 
             const text = document.createElement('span');
             text.textContent = rankName;
 
-
+            //Appends to sidebar
             div.appendChild(img);
             div.appendChild(text);
-
             ranks.appendChild(div);
-
         });
     });
 }
 
 
 window.onload = function(){
+    getAgents();
+    getTopWeapons();
     getRanks();
 }
